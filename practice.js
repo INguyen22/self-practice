2ff2e32f1bb57edbd8a51d69c7e479185a49fc9f
Ivy Nguyen <animeweirdo24@gmail.com>
2022-03-30 12:04:38 -0600

    Practice section1

---
+++ practice.js
@@ -0,0 +1,30 @@
+//Concatenation
+var speedy = 'quick red fox';
+var slowPoke = 'lazy brown dog';
+console.log('the ' + speedy + ' jumped over the ' + slowPoke);
+
+speedy = 'hare';
+slowPoke = 'tortoise'
+console.log('in a predictable result, the ' + slowPoke + ' beat the ' + speedy);
+
+//Data Types
+console.log('99 bottles of pop on the wall...');
+
+console.log(6 * 53);
+console.log(20 / 4);
+console.log(10 % 54);
+
+console.log('hello' === 'Hello');
+console.log(3 !== 4);
+console.log(4 <= 5);
+
+//Interpolation
+console.log(`the ${speedy} jumped over the ${slowPoke}`);
+
+//variables
+var warning = 'harry must not return to hogwarts'
+console.log(warning);
+
+var healthySnacks = 6;
+var junkFood = 8;
+console.log(healthySnacks + junkFood);
